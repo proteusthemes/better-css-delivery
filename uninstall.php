0338aec179d5be549b93971483f204eaf98084be
Primoz Cigler <primoz@proteusnet.com>
2016-01-12 14:22:17 +0100

    Added uninstall script.

---
+++ uninstall.php
@@ -0,0 +1,17 @@
+<?php
+
+// https://developer.wordpress.org/plugins/the-basics/uninstall-methods/
+
+// If uninstall is not called from WordPress, exit
+if ( !defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+	exit();
+}
+
+$bcd_options = array(
+	'bcd-css-handles',
+	'bcd-critical-css',
+);
+
+foreach ( $bcd_options as $option ) {
+	delete_option( $option_name );
+}
